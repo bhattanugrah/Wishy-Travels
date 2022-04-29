304307dfe2dcf00241eb5b3b99cf3da52298110f
Anugrah Bhatt <bhattanugrah@gmail.com>
2022-04-29 18:16:05 +0530

    changes

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Header } from "../layout";
 import About from "./aboutPage";
-import { ToastContainer } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 import {Row, Col, Card} from "react-bootstrap";
 import Carousel from "react-elastic-carousel";
 
 
 const Home = () =>{
+
+    const breakPoints = [
+        { width: 1, itemsToShow: 1 },
+        { width: 550, itemsToShow: 2 },
+        { width: 768, itemsToShow: 3 },
+        { width: 1200, itemsToShow: 3 },
+      ];
+      
+    
+    const[packages, setPackages] = useState([]);
+
+    useEffect(() => {
+      const apiResponse = fetch('http://localhost:3000/packages');
+      if(!apiResponse){
+        toast.error('Cannot Obtain the list')
+      }
+      else{
+        apiResponse.then((response) => response.json())
+        .then((responseJSON) =>{
+            setPackages(responseJSON)
+        })
+      }
+    },[])
+    
+
+
 
     onscroll = () => {
         if(window.scrollY > 600){
@@ -43,16 +69,15 @@ const Home = () =>{
                 </div>
                 <div className="packages-carousel">
                     <Row xs={1} md={2} className="g-4 justify-content-center">
-                        <Carousel itemsToShow={3}>
-                            {Array.from({ length: 12}).map((_, idx) => (
+                        <Carousel breakPoints={breakPoints} >
+                            {packages.map((items) => (
                                 <Col md={10}>
                                     <Card>
                                         <Card.Img variant="top" className="card-image" src="https://images.unsplash.com/photo-1611231832033-c3d79cbf2777?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" />
                                         <Card.Body>
-                                            <Card.Title>Kedarkantha</Card.Title>
+                                            <Card.Title>{items.name}</Card.Title>
                                             <Card.Text>
-                                                This is a longer card with supporting text below as a natural
-                                                lead-in to additional content. This content is a little bit longer.
+                                                ₹{items.price}
                                             </Card.Text>
                                         </Card.Body>
                                     </Card>
